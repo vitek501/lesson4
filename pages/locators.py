5b904c985fa4e64eb160693110830a2da05b70f9
vitek501 <vitek.kurdyukov@gmail.com>
2021-09-15 12:14:16 +0500

    add test_product_page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, ".login_form")
     REGISTRATION_LINK = (By.CSS_SELECTOR, ".register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    ALLERT_PRODUCT = (By.CSS_SELECTOR, ".alertinner strong")
