88535c699ef8d3a60a87fce381c15199ab4dc8c4
vitek501 <vitek.kurdyukov@gmail.com>
2021-09-19 13:56:17 +0500

    lesson 4.3.5

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,15 @@
+import pytest
 from pages.product_page import ProductPage
+import time
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('num', [*range(7),
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  *range(8,10)])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
     page = ProductPage(browser, link)   
     page.open()                      # открываем страницу
     page.test_purchase_book()
     page.solve_quiz_and_get_code()
     page.check_purchase()
+
